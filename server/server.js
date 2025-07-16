134ec115f17cdc7075a6aaad998034a52c56d2ee
itsmeayush26 <ayushsingh0255@gmail.com>
2025-07-16 23:08:11 +0530

    changes for render deploy

--- server/server.js
+++ server/server.js
@@ -48,15 +48,6 @@ app.use("/api/messages", messageRouter);
 //connect to mongodb
 await connectDB();
 
+const PORT = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV != "production"){
-  const PORT = process.env.PORT || 5000;
-  
-  server.listen(PORT, () => console.log("server is running on PORT:" + PORT));
-
-}
-
-//exporting server for vercel --->>for deployment 
-
-
-export default server;
+server.listen(PORT, () => console.log("server is running on PORT:" + PORT));
